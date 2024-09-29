0c1dd5fdebec5a7832b004e049bccb67cb6eb605
xxx <xxx@xxx.com>
2024-09-29 17:46:12 +0800

    update

---
+++ build/CMakeFiles/wboard_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wboard_autogen"
+  "wboard_autogen/mocs_compilation.cpp"
+  "wboard_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/wboard_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/wboard_autogen.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for wboard_autogen.
+# This may be replaced when dependencies are built.
